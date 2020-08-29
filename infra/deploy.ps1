17f0be052a50cadf64fde3945aec11832327b633
Paulo Henrique Silva Pinto <paulohsilvapinto@gmail.com>
2020-08-29 14:14:46 -0300

    Improvements to deploy script

--- infra/deploy.ps1
+++ infra/deploy.ps1
@@ -7,18 +7,27 @@ Param
 
 $ErrorActionPreference = "Stop"
 
+Set-Location $(Split-Path $MyInvocation.MyCommand.Path)
+Write-Output "#--- Initiating CloudFormation Deployment ---#"
 
 Remove-Item -recurse -ErrorAction Ignore .aws-sam
+Write-Output "Reformatting Python code."
 yapf --in-place --recursive ../lambdas
+Write-Output "Checking python code."
 flake8 ../lambdas
 
-aws s3 mb s3://phsp-dlg-artifacts-$environment
+$properties = ConvertFrom-StringData (Get-Content ./$environment.properties -Raw)
+$artifactsBucket = $properties.'S3ArtifactsBucketName'
 
+Write-Output "Verifying if bucket $artifactsBucket exists."
+aws s3 mb s3://$artifactsBucket
+
+Write-Output "Deploying CloudFormation Template"
 sam build
 
 sam deploy `
-    --s3-bucket phsp-dlg-artifacts-$environment `
-    --stack-name phsp-dlg-datalake-$environment `
+    --s3-bucket $properties.'S3ArtifactsBucketName' `
+    --stack-name $properties.'StackName' `
     --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND `
     --parameter-overrides $(Get-Content "$environment.properties")
 
