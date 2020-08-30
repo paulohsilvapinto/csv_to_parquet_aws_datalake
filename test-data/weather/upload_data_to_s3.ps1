4042f480cf1fc9c5fc58347047c81015373a5604
Paulo Henrique Silva Pinto <paulohsilvapinto@gmail.com>
2020-08-30 02:23:32 -0300

    Changing log messages

--- test-data/weather/upload_data_to_s3.ps1
+++ test-data/weather/upload_data_to_s3.ps1
@@ -8,15 +8,15 @@ Param
 $ErrorActionPreference = "Stop"
 
 Set-Location $(Split-Path $MyInvocation.MyCommand.Path)
-Write-Output "Starting load of Weather data."
+Write-Output "`n`nLoading Weather data."
 
 $properties = ConvertFrom-StringData (Get-Content ../../infra/$environment.properties -Raw)
 $bucket = $properties.'S3RawBucketName'
 
 $files = Get-ChildItem . -Filter *.csv | Select-Object -Expand FullName
 foreach($file in $files){
-    Write-Output "Loading file $file to $bucket."
+    Write-Output "`nLoading file $file to $bucket."
     aws s3 cp $file s3://$bucket/csv_to_analytics/weather/ --metadata file://metadata.json
 }
 
-Write-Output "Load of Weather data is finished."
+Write-Output "`nWeather data was loaded successfully."
